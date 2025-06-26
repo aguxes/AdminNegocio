d883f3602c9eadd122332f8db5f33032ba6f8611
Agustin Arrambide <arrambide.agustin@gmail.com>
2025-06-25 23:03:45 -0300

    UI upgrades

--- src/main/java/View/App.java
+++ src/main/java/View/App.java
@@ -3,6 +3,7 @@ package View;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("UIAdminNegocio.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 860, 720);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/View/UIAdminNegocio.fxml"));
+
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1300, 800);
+
+        // 👉 Conectás el CSS acá
+        scene.getStylesheets().add(App.class.getResource("/Estilos/estilos.css").toExternalForm());
+
         stage.setTitle("Que lees virgo");
         stage.setScene(scene);
         stage.show();
--- src/main/java/View/AppController.java
+++ src/main/java/View/AppController.java
@@ -1,6 +1,7 @@
 package View;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -27,5 +28,11 @@ public class AppController {
         String datos = VentanaVentas.obtenerVentasComoTexto();
         outputArea.setText(datos);
 
+    }
+
+    public void mostrarInventario(ActionEvent actionEvent) {
+    }
+
+    public void mostrarReportes(ActionEvent actionEvent) {
     }
 }
