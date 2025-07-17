6cdb870b0434cffdcd77514eeb0fbfd5df29e018
Agustin Arrambide <arrambide.agustin@gmail.com>
2025-07-16 23:40:13 -0300

    Litlle changes en mapper

--- src/main/java/util/Mapper.java
+++ src/main/java/util/Mapper.java
@@ -57,7 +57,6 @@ public class Mapper {
 
     // Mapeo de Venta
     // ========================================
-
     //Formato de sql para guardar y leer la fecha
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
@@ -72,26 +71,27 @@ public class Mapper {
         LocalDateTime fecha = LocalDateTime.parse(fechaTexto, FORMATTER);
 
         return new Venta(
-                rs.getInt("nFactura"),               // ID de la venta
-                rs.getInt("idE"),                    // ID del empleado
-                rs.getInt("idC"),                    // ID del cliente
+                rs.getInt("nFactura"),
+                rs.getInt("idE"),
+                rs.getInt("idC"),
                 fecha,
-                rs.getString("medio_pago"),          // Forma de pago (alias del SELECT)
-                rs.getBigDecimal("total"),           // Total
-                "",
-                rs.getString("cliente_nombre"),      // nombre del cliente (JOIN con Persona)
-                rs.getString("empleado_nombre")      // nombre del empleado (JOIN con Persona)
+                rs.getString("medio_pago"),
+                rs.getBigDecimal("total"),
+                rs.getString("producto_nombre"), // ahora es producto, no notas
+                rs.getString("cliente_nombre"),
+                rs.getString("empleado_nombre")
         );
+
     }
 
     // Este método sirve si vas a insertar una nueva venta
     public static void setVenta(PreparedStatement stmt, Venta venta) throws SQLException {
-        //  stmt.setInt(1, venta.getIdProducto()); // ahora sí existe
+        stmt.setInt(1, venta.getIdProducto()); // ahora sí existe
         stmt.setInt(2, venta.getIdEmpleado());
         stmt.setInt(3, venta.getIdCliente());
         stmt.setString(4, venta.getFecha().format(FORMATTER));
-        //   stmt.setInt(5, venta.getIdFormaDePago());  // ahora existe
-        //   stmt.setBigDecimal(6, venta.getSubtotal()); // ahora existe
+        stmt.setInt(5, venta.getIdFormaDePago());  // ahora existe
+        stmt.setBigDecimal(6, venta.getSubtotal()); // ahora existe
         stmt.setBigDecimal(7, venta.getImporteTotal());
     }
 }
