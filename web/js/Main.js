6ab30d1b671947e0e906b7b3e40e89ea886dc138
Agustin Arrambide <arrambide.agustin@gmail.com>
2025-07-30 00:42:05 -0300

    Now we show clients better, its beautifull

--- web/js/Main.js
+++ web/js/Main.js
@@ -3,15 +3,27 @@ function saludar() {
     mensaje.textContent = "¡Hola desde JavaScript! Todo anda bien 😎";
 }
 async function cargarClientes() {
-    const response = await fetch("http://localhost:4567/clientes")
+    const response = await fetch("http://localhost:4567/clientes");
     const data = await response.json();
 
     const contenedor = document.getElementById("mensaje");
     contenedor.innerHTML = "";
 
     data.forEach(cliente => {
-        const div = document.createElement("div");
-        div.textContent = `Cliente: ${cliente.nombre} - DNI: ${cliente.DNI}`;
-        contenedor.appendChild(div);
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("tarjeta");
+
+        tarjeta.innerHTML = `
+            <p><strong>ID:</strong> ${cliente.id}</p>
+            <p><strong>DNI:</strong> ${cliente.DNI}</p>
+            <p><strong>Nombre:</strong> ${cliente.nombre} ${cliente.apellido}</p>
+            <p><strong>Tipo:</strong> ${cliente.tipo.descripcion}</p>
+            <p><strong>Compras:</strong> ${cliente.cantCompras}</p>
+            <p><strong>Teléfono:</strong> ${cliente.telefono?.telefono ?? 'No registrado'}</p>
+        `;
+
+        contenedor.appendChild(tarjeta);
     });
 }
+
+
