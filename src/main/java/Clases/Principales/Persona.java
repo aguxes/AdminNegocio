d46b702e63bb36b1dd0c2a1f92d89aa834192166
Agustin <agustinbaied1@gmail.com>
2025-08-12 11:06:27 -0300

    Now yes

--- src/main/java/Clases/Principales/Persona.java
+++ src/main/java/Clases/Principales/Persona.java
@@ -1,17 +1,21 @@
 package Clases.Principales;
 
+import Clases.Extras.Telefono;
+
 public class Persona {
     protected int DNI;
     protected String nombre;
     protected String apellido;
+    private Telefono telefono;
 
     private static int cont = 0;//Contador de personas registradas en el programa
 
-    public Persona(int dni, String nombre, String apellido) {
+    public Persona(int dni, String nombre, String apellido, Telefono telefono) {
         cont++;
         this.DNI = dni;
         this.nombre = nombre;
         this.apellido = apellido;
+        this.telefono = telefono;
     }
     public Persona()
     {
@@ -26,8 +30,13 @@ public class Persona {
     public int getDNI() { return DNI; }
     public String getNombre() { return nombre; }
     public String getApellido() { return apellido; }
+    public Telefono getTelefono() { return telefono; }
 
     public void setDNI(int DNI) { this.DNI = DNI; }
     public void setNombre(String nombre) { this.nombre = nombre; }
     public void setApellido(String apellido) { this.apellido = apellido; }
+    public void setTelefono(Telefono telefono) { this.telefono = telefono; }
+
+    public String getTelefonoStr() { return telefono != null ? String.valueOf(telefono.getTelefono()) : ""; }
+    // Caso de uso de esta cosa??
 }
